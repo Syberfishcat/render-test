62495c799570f85e17d2ea58227479e213629d69
sybercat <sybercat@163.com>
2026-08-19 12:51:02 +0300

    Refactoring the route responsible for adding a note

--- controllers/notes.js
+++ controllers/notes.js
@@ -25,18 +25,15 @@ notesRouter.delete('/:id', (request, response, next) => {
     .catch(error => next(error))
 })
 
-notesRouter.post('/', (request, response, next) => {
+notesRouter.post('/', async (request, response) => {
   const body = request.body
   const note = new Note({
     content: body.content,
     important: body.important || false,
   })
 
-  note.save()
-    .then(savedNote => {
-      response.json(savedNote)
-    })
-    .catch(error => next(error))
+  const savedNote = await note.save()
+  response.status(201).json(savedNote)
 })
 
 notesRouter.put('/:id', (request, response, next) => {
