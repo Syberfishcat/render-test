fc1c84e9b92bf079bae732c909184938271418eb
sybercat <sybercat@163.com>
2026-08-21 11:42:51 +0300

    Creating users

--- tests/note_api.test.js
+++ tests/note_api.test.js
@@ -5,8 +5,64 @@ const supertest = require('supertest')
 const app = require('../app')
 const helper = require('./test_helper')
 const Note = require('../models/note')
+const bcrypt = require('bcrypt')
+const User = require('../models/user')
 
 const api = supertest(app)
+
+describe('when there is initially one user in db', () => {
+  beforeEach(async () => {
+    await User.deleteMany({})
+
+    const passwordHash = await bcrypt.hash('sekret', 10)
+    const user = new User({ username: 'root', passwordHash })
+
+    await user.save()
+  })
+
+  test('creation succeeds with a fresh username', async () => {
+    const usersAtStart = await helper.usersInDb()
+
+    const newUser = {
+      username: 'mluukkai',
+      name: 'Matti Luukkainen',
+      password: 'salainen',
+    }
+
+    await api
+      .post('/api/users')
+      .send(newUser)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+
+    const usersAtEnd = await helper.usersInDb()
+    assert.strictEqual(usersAtEnd.length, usersAtStart.length + 1)
+
+    const usernames = usersAtEnd.map(u => u.username)
+    assert(usernames.includes(newUser.username))
+  })
+
+  test('creation fails with proper statuscode and message if username already taken', async () => {
+    const usersAtStart = await helper.usersInDb()
+
+    const newUser = {
+      username: 'root',
+      name: 'Superuser',
+      password: 'salainen',
+    }
+
+    const result = await api
+      .post('/api/users')
+      .send(newUser)
+      .expect(400)
+      .expect('Content-Type', /application\/json/)
+
+    const usersAtEnd = await helper.usersInDb()
+    assert(result.body.error.includes('expected `username` to be unique'))
+
+    assert.strictEqual(usersAtEnd.length, usersAtStart.length)
+  })
+})
 
 describe('when there is initially some notes saved', () => {
   beforeEach(async () => {
@@ -30,7 +86,7 @@ describe('when there is initially some notes saved', () => {
   test('a specific note is within the returned notes', async () => {
     const response = await api.get('/api/notes')
 
-    const contents = response.body.map(e => e.content)
+    const contents = response.body.map((e) => e.content)
     assert(contents.includes('HTML is easy'))
   })
 
@@ -76,7 +132,7 @@ describe('when there is initially some notes saved', () => {
       const notesAtEnd = await helper.notesInDb()
       assert.strictEqual(notesAtEnd.length, helper.initialNotes.length + 1)
 
-      const contents = notesAtEnd.map(n => n.content)
+      const contents = notesAtEnd.map((n) => n.content)
       assert(contents.includes('async/await simplifies making async calls'))
     })
 
@@ -100,8 +156,8 @@ describe('when there is initially some notes saved', () => {
 
       const notesAtEnd = await helper.notesInDb()
 
-      const ids = notesAtEnd.map(n => n.id)
-      assert(!ids.includes(noteToDelete.id))
+      const contents = notesAtEnd.map((n) => n.content)
+      assert(!contents.includes(noteToDelete.content))
 
       assert.strictEqual(notesAtEnd.length, helper.initialNotes.length - 1)
     })
